243709a8e3da056184989beed6be19ce229312bd
AJLoveChina <735267452@qq.com>
2022-10-29 22:30:12 +0800

    chore: mui

--- src/comps/dashboard/DashboardCard.tsx
+++ src/comps/dashboard/DashboardCard.tsx
@@ -1,30 +1,56 @@
 import { CSSProperties } from "react"
 
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+
 export function DashboardCard(props: {
   style?: CSSProperties
   img: string
   title: string
-  onClick: () => void
+  onClick?: () => void
 }) {
   return (
-    <div
-      style={{
-        width: "260px",
-        height: "330px",
-        border: "2px solid #eee",
-        padding: "20px",
-        boxShadow: "0 0 2px 2px lightblue",
-        ...props.style,
-      }}
-      onClick={props.onClick}
-    >
-      <div style={{ width: "220px", height: "220px" }}>
-        <img src={props.img} alt="" style={{ width: "100%" }} />
-      </div>
-      <p style={{ width: "100%", textAlign: "center", fontSize: "22px" }}>
-        {" "}
-        {props.title}
-      </p>
+    // <div
+    //   style={{
+    //     width: "260px",
+    //     height: "330px",
+    //     border: "2px solid #eee",
+    //     padding: "20px",
+    //     boxShadow: "0 0 2px 2px lightblue",
+    //     ...props.style,
+    //   }}
+    //   onClick={props.onClick}
+    // >
+    //   <div style={{ width: "220px", height: "220px" }}>
+    //     <img src={props.img} alt="" style={{ width: "100%" }} />
+    //   </div>
+    //   <p style={{ width: "100%", textAlign: "center", fontSize: "22px" }}>
+    //     {" "}
+    //     {props.title}
+    //   </p>
+    // </div>
+
+    <div style={props.style}>
+      <Card sx={{ maxWidth: 500, width: 300 }}>
+        <CardMedia component="img" height="140" image={props.img} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography> */}
+        </CardContent>
+        <CardActions>
+          <Button size="small">打开</Button>
+          {/* <Button size="small">Learn More</Button> */}
+        </CardActions>
+      </Card>
     </div>
   )
 }
--- src/comps/dashboard/dashboard.tsx
+++ src/comps/dashboard/dashboard.tsx
@@ -1,55 +1,68 @@
-import { useState } from "react"
+import { CSSProperties, useState } from "react"
 import { CategoryEnum } from "../../common/common"
 import { Car } from "../car/Car"
 import { Food } from "../Food"
 import { HomeSVG } from "../../svgs/svgs"
 import { DashboardCard } from "./DashboardCard"
 import "./dashboard.css"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 export function Dashboard() {
-  const [cat, setCat] = useState<CategoryEnum | undefined>(undefined)
   const cards = [
     {
       img: "img/dashboard/food.jpg",
       title: "美味的食物",
       cat: CategoryEnum.FOOD,
+      path: "/food",
     },
     {
       img: "img/dashboard/boli.jpg",
       title: "交通工具",
       cat: CategoryEnum.CAR,
+      path: "/car",
     },
   ]
 
+  const homeBackPosCss: CSSProperties = {
+    position: "fixed",
+    left: "20px",
+    top: "20px",
+    zIndex: 999,
+  }
+
   return (
-    <>
-      {cat === undefined && (
-        <div className={"dashboard"}>
-          {cards.map((card) => {
-            return (
-              <DashboardCard
-                style={{ margin: "20px" }}
-                img={card.img}
-                title={card.title}
-                onClick={() => setCat(card.cat)}
-              />
-            )
-          })}
-        </div>
-      )}
-      {cat !== undefined && (
-        <HomeSVG
-          style={{
-            position: "fixed",
-            left: "20px",
-            top: "20px",
-            zIndex: 999,
-          }}
-          onClick={() => setCat(undefined)}
-        />
-      )}
-      {cat === CategoryEnum.FOOD && <Food />}
-      {cat === CategoryEnum.CAR && <Car />}
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <div className={"dashboard"}>
+            {cards.map((card) => {
+              return (
+                <Link to={card.path} key={card.path}>
+                  <DashboardCard
+                    style={{ margin: "20px" }}
+                    img={card.img}
+                    title={card.title}
+                  />
+                </Link>
+              )
+            })}
+          </div>
+        </Route>
+
+        <Route path="/food">
+          <Food />
+          <Link to="/">
+            <HomeSVG style={homeBackPosCss} />
+          </Link>
+        </Route>
+
+        <Route path="/car">
+          <Car />
+          <Link to="/">
+            <HomeSVG style={homeBackPosCss} />
+          </Link>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
